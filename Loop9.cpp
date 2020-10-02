d7c89d3ec2114cfc4e0f77b331d721b058f5fa4f
Muskanvk12 <62818492+Muskanvk12@users.noreply.github.com>
2020-10-02 22:25:38 +0530

    Update Loop9.cpp
    Made the code more readable

--- Loop9.cpp
+++ Loop9.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-	int sum = 0;
-	for (int i=0;  i<= 5;i++){
-    sum = sum+i;
-    cout<<sum<<endl;
-	}
-	return 0;
+    int sum = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        sum += i;
+        cout << sum << endl;
+    }
+    return 0;
 }
